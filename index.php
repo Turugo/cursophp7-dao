88f2e8e10335ba55d8deddbd0f8854ae3361d9a4
Vitor Souza <vitorhugops23@gmail.com>
2018-03-12 16:22:46 -0300

    comando delete

--- index.php
+++ index.php
@@ -2,35 +2,42 @@
 
 require_once("config.php");
 
-//carrega um usuario
+# carrega um usuario
 //$root = new Usuario();
 //$root->loadbyId(1);
 //echo $root;
 
-
-//carrega uma lista de usuarios
+# carrega uma lista de usuarios
 //$lista = Usuario::getList();
 //echo json_encode($lista);
 
-//Carrega uma lista de usuarios buscando pelo login
+# Carrega uma lista de usuarios buscando pelo login
 //$search = Usuario::search("o");
 //echo json_encode($search);
 
-//carrega um usuario usando login e senha
+# carrega um usuario usando login e senha
 //$usuario = new Usuario();
 //$usuario->login("jojo", "123456");
 //echo $usuario;
 
-//criando um usuario
+# criando um usuario
 //$aluno = new Usuario("aluno", "@lun0");
 //$aluno->insert();
 //echo $aluno;
 
+
+# alterar um usuario
+//$usuario = new Usuario();
+//$usuario->loadbyId(7);
+//$usuario->update("professor", "profe");
+//echo $usuario;
+
+# deletar um suario
 $usuario = new Usuario();
 
 $usuario->loadbyId(7);
 
-$usuario->update("professor", "profe");
+$usuario->delete();
 
 echo $usuario;
 
